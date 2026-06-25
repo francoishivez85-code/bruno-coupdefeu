b6fefe179b90107e4a665493a74504b9760532c7
Fabrice Solsona <fsolsona@hotmail.com>
2026-06-25 10:25:06 +0200

    Devise + Brand + Bruno=>Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Met le videur en place
+  before_action :authenticate_user!
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
