c468d667196bed4ab711d91bcb80c5db397361f0
hivez <francoishivez85@gmail.com>
2026-06-30 07:32:39 +0200

    Chat Bruno : même photo que la navbar (Kitt Le Wagon) pour l'avatar user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     hash = Digest::MD5.hexdigest(user.email.downcase.strip)
     "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=404"
   end
+
+  # Même source que la navbar — à remplacer par user.avatar_url quand on aura la colonne
+  def user_avatar_src(user, size: 80)
+    "https://kitt.lewagon.com/placeholder/users/ssaunier"
+  end
 end
