8309c60aaefd7ef2df23cd54cd29b5c81a21ad41
hivez <francoishivez85@gmail.com>
2026-06-30 07:26:06 +0200

    Avatar utilisateur partagé navbar+chat : Gravatar → initiale email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def user_gravatar_url(user, size: 80)
+    hash = Digest::MD5.hexdigest(user.email.downcase.strip)
+    "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=404"
+  end
 end
